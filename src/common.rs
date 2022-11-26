4a78fcf4e6d293cd13a18e31dedeaaeacb4a786d
Azertinv <azertinv@protonmail.com>
2022-11-26 22:27:12 +0100

    fixed CT_ALPHABET initialization

--- src/common.rs
+++ src/common.rs
@@ -1,17 +1,13 @@
+use array_const_fn_init::array_const_fn_init;
+
 pub const CT_ALPHABET_SIZE: u8 = 83;
 pub const CT_ALPHABET_USIZE: usize = CT_ALPHABET_SIZE as usize;
 
-pub const CT_ALPHABET: [u8; CT_ALPHABET_USIZE] = [
-     0,  1,  2,  3,  4,  5,  6,  7,  8,  9,
-    10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-    20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-    30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
-    40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
-    50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
-    60, 61, 62, 63, 64, 65, 66, 67, 68, 69,
-    70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
-    80, 81, 82,
-];
+const fn usize_to_u8(i: usize) -> u8 {
+    i as u8
+}
+
+pub const CT_ALPHABET: [u8; CT_ALPHABET_USIZE] = array_const_fn_init!(usize_to_u8; 83);
 
 pub const READABLE_OFFSET: u8 = 32;
 
